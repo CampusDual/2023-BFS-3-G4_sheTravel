e25f6f436efdb8c8f94244f4b94d74c2ed79f750
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-02-14 14:09:55 +0100

    Assets resources path

--- cd2023bfs3g4-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2023bfs3g4-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -3,7 +3,7 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   // { id: 'myprofile', name: 'MY PROFILE', icon: 'profile', opened: true,
   //   items: [
-  // { id: 'userinfo', name: 'USER', user: 'Profile', avatar: '../../assets/images/user_profile.png' }
+  // { id: 'userinfo', name: 'USER', user: 'Profile', avatar: 'assets/images/user_profile.png' }
   // ]},
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'destination', name: 'DESTINATION', icon: 'explore', route: '/main/communities' },
